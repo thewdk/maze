e3b5e6c04f89acd3008a3623d6d81789c2c6fd7e
Daniel Watchman <dannietortee@gmail.com>
2024-07-17 21:25:30 +0100

    all set

---
+++ headers/game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <float.h>
+#include "defs.h"
+#include "graphics.h"
+#include "map.h"
+#include "player.h"
+#include "ray.h"
+#include "wall.h"
+#include "sprite.h"
+#include "textures.h"
+#include "upng.h"
+#include "utils.h"
+#endif
